541d5afca16b1274892be2a87207367fb164deea
Konstantin Komelin <konstantin.komelin@gmail.com>
2025-01-15 11:16:16 +0100

    frontend: Extracted the Main component to a separate file to shut the linter up.

---
+++ packages/frontend/src/components/Main.tsx
@@ -0,0 +1,36 @@
+import '@mysten/dapp-kit/dist/index.css'
+import '@radix-ui/themes/styles.css'
+import '@suiware/kit/main.css'
+import SuiProvider from '@suiware/kit/SuiProvider'
+import { FC, StrictMode } from 'react'
+import App from '~~/components/App'
+import { APP_NAME } from '~~/config/main'
+import { getThemeSettings } from '~~/helpers/theme'
+import useNetworkConfig from '~~/hooks/useNetworkConfig'
+import ThemeProvider from '~~/providers/ThemeProvider'
+import '~~/styles/index.css'
+import { ENetwork } from '~~/types/ENetwork'
+
+const themeSettings = getThemeSettings()
+
+const Main: FC = () => {
+  const { networkConfig } = useNetworkConfig()
+
+  return (
+    <StrictMode>
+      <ThemeProvider>
+        <SuiProvider
+          customNetworkConfig={networkConfig}
+          defaultNetwork={ENetwork.LOCALNET}
+          walletAutoConnect={false}
+          walletStashedName={APP_NAME}
+          themeSettings={themeSettings}
+        >
+          <App />
+        </SuiProvider>
+      </ThemeProvider>
+    </StrictMode>
+  )
+}
+
+export default Main
--- packages/frontend/src/helpers/theme.ts
+++ packages/frontend/src/helpers/theme.ts
@@ -1,4 +1,5 @@
 import { TTheme } from '~~/types/TTheme'
+import { darkTheme, lightTheme } from '../config/themes'
 
 export const detectBrowserTheme = (): TTheme => {
   return !('theme' in localStorage) &&
@@ -6,3 +7,20 @@ export const detectBrowserTheme = (): TTheme => {
     ? 'dark'
     : 'light'
 }
+
+export const getThemeSettings = () => [
+  {
+    // Default to light theme.
+    variables: lightTheme,
+  },
+  {
+    // React to the color scheme media query.
+    mediaQuery: '(prefers-color-scheme: dark)',
+    variables: darkTheme,
+  },
+  {
+    // Reacts to the dark class.
+    selector: '.dark',
+    variables: darkTheme,
+  },
+]
