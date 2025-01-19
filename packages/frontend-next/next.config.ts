2ff44bba8d35288886ded79505119ea710c69268
Konstantin Komelin <konstantin.komelin@gmail.com>
2025-01-19 21:19:19 +0100

    frontend-next: Created frontend-next package with the Next.js version of the app.

---
+++ packages/frontend-next/next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  distDir: "dist",
+  reactStrictMode: true,
+  transpilePackages: [
+    "@mysten/dapp-kit",
+    "@mysten/sui.js",
+    "@mysten/wallet-standard",
+    "@suiware/kit",
+    "@radix-ui/themes",
+    "@radix-ui/react-select",
+    "@radix-ui/react-toggle",
+  ],
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), "encoding"];
+    return config;
+  },
+};
+
+export default nextConfig;
