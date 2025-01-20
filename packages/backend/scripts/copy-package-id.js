d9396c34633a8ddd6a45b1d0bfadf6b9fadbe62b
Konstantin Komelin <konstantin.komelin@gmail.com>
2025-01-20 14:20:02 +0100

    backend: Adapted copy-package-id script to support Next.js frontend automatically.

--- packages/backend/scripts/copy-package-id.js
+++ packages/backend/scripts/copy-package-id.js
@@ -3,7 +3,7 @@
 /**
  * The script copies the deployed package ID from the corresponding Suibase network file to .env.local of the frontend package,
  * which is then read by the app.
- * 
+ *
  * The default network is localnet. To change it, pass "-n [NETWORK_TYPE]" through console.
  */
 
@@ -16,19 +16,21 @@ const DEPLOYED_MODULE_NAME = "greeting";
 
 const main = async () => {
   const network = getNetworkFromArgs();
-  const sourceFile = sourceFilePath(network, DEPLOYED_MODULE_NAME);
-  const targetFile = targetFilePath();
 
-  // Read package ID from SuiBase packageId file.
-  const packageId = await readPackageId(sourceFile);
+  // Read package ID from Suibase packageId file.
+  const packageId = await readPackageId(network);
 
   // Create .env.local file if it doesn't exist.
+  const targetFile = targetFilePath();
   await createFileIfNecessary(targetFile);
 
-  // Add VITE_[network]_CONTRACT_PACKAGE_ID variable to .env.local or update its value if it exists.
+  // Check whether the frontend is Next.js-powered to decide what environment variable name format to use.
+  const isNextJs = await isNextJsProject();
+
+  // Add Move package ID to .env.local or update it if it already exists.
   await setEnvVar(
     targetFile,
-    `VITE_${network.toUpperCase()}_CONTRACT_PACKAGE_ID`,
+    `${isNextJs ? "NEXT_PUBLIC" : "VITE"}_${network.toUpperCase()}_CONTRACT_PACKAGE_ID`,
     packageId
   );
 };
@@ -59,10 +61,11 @@ const getNetworkFromArgs = () => {
 /**
  * Read package ID from SuiBase packageId file.
  *
- * @param {string} sourceFile
+ * @param {string} network
  * @returns
  */
-const readPackageId = async (sourceFile) => {
+const readPackageId = async (network) => {
+  const sourceFile = sourceFilePath(network, DEPLOYED_MODULE_NAME);
   const data = await promises.readFile(sourceFile, "utf8");
   return JSON.parse(data)[0];
 };
@@ -92,6 +95,20 @@ const setEnvVar = async (envFilePath, name, value) => {
   await envFileWriter.parse();
   envFileWriter.set(name, value);
   await envFileWriter.save();
+};
+
+/**
+ * Check if next.config.ts exists in the frontend package.
+ *
+ * @returns {boolean}
+ */
+const isNextJsProject = async () => {
+  try {
+    await promises.stat(path.join(process.cwd(), "../frontend/next.config.ts"));
+    return true;
+  } catch {
+    return false;
+  }
 };
 
 // Main entry point.
